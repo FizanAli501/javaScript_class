90472c46ef112cbde9e4fd6860a3784b3fc28a10
FizanAli501 <fa141556@gmail.com>
2024-10-14 21:42:27 +0500

    new commint

---
+++ index.js
@@ -0,0 +1,87 @@
+// prompt("enter your choice");
+// prompt is used to get the value from the user . It is a built-in function in JavaScript. It is used to get the value from the user
+// alert("we are facing  some technical issues, please try again later");
+// alert is used to show the alert message 
+
+// confirm("are you sure to del te this file");
+// confirm is used to show the confirm message. It is used to get the value from the user
+// let num1 =parseInt(prompt("enter the first number"));
+// let num2 =parseInt(prompt("enter the secound number"));
+// let c =  num1 + num2;
+// alert(c)
+
+// let age = parseInt(prompt("enter your age"))
+// if(age>=18){
+//     alert("you are eligible to vote")
+//     }else{
+//         alert("you are not eligible to vote")
+//         }
+
+// let day =parseInt(prompt("enter day of week"))
+// if(day==1){
+//     alert("monday")
+// }else if(day==2){
+//     alert("tuesday")
+// }else if(day ==3){
+//     alert("wednesday")
+// }else if (day ==4){
+//     alert("thursday")
+// }else if(day ==5){
+//     alert("friday")
+// }else if(day ==6){
+//     alert("saturday")
+// }else if(day==7){
+//     alert("sunday")
+// }else{
+//     alert("invalid day")
+// }
+
+// let days = parseInt(prompt("enter day of week"))
+// switch(days){
+//     case 1:
+//         alert("monday")
+//         break;
+//     case 2:
+//         alert("tuesday")
+//         break;
+//     case 3:
+//         alert("wednesday")
+//         break;
+//     case 4:
+//         alert("thursday")
+//         break;
+//     case 5:
+//         alert("friday")
+//         break;
+//     case 6:
+//         alert("saturday")
+//         break;
+//     case 7:
+//         alert("sunday")
+//         break;
+//     default:
+//         alert("invalid day")
+// }
+
+// let name = "ahmad";
+// let name1 = "daniyal";
+
+// if(name =="ahmad" &&  name1=="daniyal"){
+//     alert("both names are same")}
+//     else{
+//         alert("names are different")
+//     }
+
+// let email =  "ahmad@gmail.com";
+// let password = "123456";
+// let confirmPassword = "123456";
+// let newemail = prompt("enter your email")
+// let pasword =  prompt("enter your password")
+
+// if(email  == newemail && password == pasword){
+//     alert("login success")}
+//     else{
+//         alert("login failed")
+//         }
+
+
